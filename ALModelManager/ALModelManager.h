d83c3234f998fa898da2815e85a0cc836c26f56e
AidenJLee <entist@me.com>
2014-02-10 23:57:14 +0900

    Initial Commit

---
+++ ALModelManager/ALModelManager.h
@@ -0,0 +1,13 @@
+//
+//  ALModelManager.h
+//  ALModelManager
+//
+//  Created by HoJun Lee on 2014. 2. 10..
+//  Copyright (c) 2014년 HoJun Lee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ALModelManager : NSObject
+
+@end
---
+++ ALModelManager/ALModelManager.m
@@ -0,0 +1,13 @@
+//
+//  ALModelManager.m
+//  ALModelManager
+//
+//  Created by HoJun Lee on 2014. 2. 10..
+//  Copyright (c) 2014년 HoJun Lee. All rights reserved.
+//
+
+#import "ALModelManager.h"
+
+@implementation ALModelManager
+
+@end
